7688f8d8cde80e68161b4cf14389ccf51cacd9e9
Truong Hoang Dung <revskill09@gmail.com>
2013-05-07 08:14:46 +0700

    Add user

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :cas_authenticatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :username, :masinhvien, :name
+  # attr_accessible :title, :body
+  def cas_extra_attributes=(extra_attributes)
+    extra_attributes.each do |name, value|
+      case name.to_sym
+      when :hovaten
+        self.name = value
+      when :masinhvien
+        self.masinhvien = value
+      end
+    end
+  end
+end
